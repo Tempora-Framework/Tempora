ddd6ff7d8cf12582114ac3c54c21700b4f7a4e3a
SkyWors <skyworslegrand@gmail.com>
2025-05-23 23:24:03 +0200

    ✨ Add js payloads system

--- src/Controllers/Controller.php
+++ src/Controllers/Controller.php
@@ -7,6 +7,7 @@ class Controller {
 	private array $pageData = [];
 	private array $styles = [];
 	private array $scripts = [];
+	private array $payloads = [];
 
 	public function includeAssets(): void {
 		foreach ($this->styles as $style) {
@@ -15,6 +16,17 @@ class Controller {
 		foreach ($this->scripts as $script) {
 			echo "<script defer src=\"" . str_replace(search: ".js", replace: ".min.js", subject: $script) . "\"></script>";
 		}
+	}
+
+	public function includePayloads(): void {
+		if (empty($this->payloads))
+			return;
+
+		echo "<div id=\"payloads\" class=\"hidden\">";
+		foreach ($this->payloads as $key => $payload) {
+			echo "<div data-payload-" . $key . "=\"" . htmlspecialchars(string: $payload) . "\"></div>";
+		}
+		echo "</div>";
 	}
 
 	/**
@@ -79,6 +91,28 @@ class Controller {
 	 */
 	public function setScripts(array $scripts): self {
 		$this->scripts = $scripts;
+
+		return $this;
+	}
+
+	/**
+	 * Get the value of payloads
+	 *
+	 * @return array
+	 */
+	public function getPayloads(): array {
+		return $this->payloads;
+	}
+
+	/**
+	 * Set the value of payloads
+	 *
+	 * @param array $payloads
+	 *
+	 * @return self
+	 */
+	public function setPayloads(array $payloads): self {
+		$this->payloads = $payloads;
 
 		return $this;
 	}
